9140d8a9e4960497b4f86e34a8716e8a9039bbf4
aditi-123-ai <aditijayy@gmail.com>
2022-07-22 19:51:40 +0530

    error fixed

--- src/component/Pricing.js
+++ src/component/Pricing.js
@@ -37,20 +37,23 @@ function Pricing() {
                 </div>
                 <div className='w-80 h-full shadow-2xl rounded-lg ml-10 '>
                     <div className='w-full h-full flex items-center flex-col'>
-                        <div className='flex flex-col justify-center items-center mt-10'>
-                            <h1 className='text-3xl	'>Classic</h1>
-                            <div className='flex justify-center items-center mt-3'>
-                                <ArrowNarrowLeftIcon className='w-10'/>
-                                <ArrowNarrowRightIcon className='w-10'/>
+                        <div className='w-full h-full bg-[#6327BC]'>
+                            <div className='flex flex-col justify-center items-center mt-10 '>
+                                <h1 className='text-3xl	'>Classic</h1>
+                                <div className='flex justify-center items-center mt-3'>
+                                    <ArrowNarrowLeftIcon className='w-10'/>
+                                    <ArrowNarrowRightIcon className='w-10'/>
+                                </div>
+                                <h1 className='text-5xl'>$30/mo</h1>
                             </div>
-                            <h1 className='text-5xl'>$30/mo</h1>
                         </div>
-                        <div className='flex justify-center items-center flex-col mt-10'>
+                        
+                        <div className='flex justify-center items-center flex-col mt-5'>
                             <p className='text-slate-700 mb-3'>Easy Customizable</p>
                             <p className='text-slate-700 mb-3'>Commercia license</p>
                             <p className='text-slate-700 mb-3'>5 user license</p>
-                            <p className='text-slate-700 mb-3'>Hotline support 24/7</p>
-                            <button className='w-52 h-12 flex justify-center items-center mt-3 bg-[#ECF7FD] text-[#53BBF0] text-base'>TRY THIS PACKAGE</button>
+                            <p className='text-slate-700'>Hotline support 24/7</p>
+                            <button className='w-52 h-12 flex justify-center items-center mt-3 mb-3 bg-[#43B5EE] text-white text-base'>TRY THIS PACKAGE</button>
                         </div>
                     </div>
                 </div>
